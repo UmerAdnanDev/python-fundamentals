49a5ce32e00ec968088712925c5e0d495917e898
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-08-02 21:23:07 +0500

    op2

---
+++ Operators/Operators/LogicalOp.py
@@ -0,0 +1,21 @@
+ #Logical Operator(and,or,not)
+#and: true if all 
+isWorkingDay = True
+age = 19
+print(19>18 and 23<56) 
+print(12>12 and 12>=12)
+print(13 ==13 or 13<=13)
+print(not(isWorkingDay))
+# True
+# False
+# True
+# False
+print("\nExperiment")
+print(True + True)
+print(False + False)
+print(True+False)
+# Experiment
+# 2
+# 0
+# 1
+#meaning : True = 1 and False = 0
---
+++ Operators/Operators/membershipOp.py
@@ -0,0 +1,6 @@
+#Membership Operator
+classA_students = ["Ali","Saad","Waseem"]
+print("Ali" in classA_students)
+print("Danish" not in classA_students)
+print("Ayesha" in classA_students)
+print("Yusoof" not in classA_students)
