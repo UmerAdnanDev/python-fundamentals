715bd67debcdfdf2116a159c6b051227ce402f68
OMERAdnan5 <omeradnannovember2005@gmail.com>
2025-08-03 22:45:39 +0500

    list operations

---
+++ list/membership.py
@@ -0,0 +1,3 @@
+list0 = [1,2,3,4,5,6,7]
+print(1 in list0)
+print(8 in list0)
---
+++ list/multiplication.py
@@ -0,0 +1,2 @@
+lists = ["5",4,False,3.0]
+print(lists*3) #['5', 4, False, 3.0, '5', 4, False, 3.0, '5', 4, False, 3.0]
