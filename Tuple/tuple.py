a2fd073a4f48bb39b1b4619425741a3c73355402
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-03-30 10:54:03 +0500

    added tuple

---
+++ Tuple/tuple.py
@@ -0,0 +1,8 @@
+tuple1 = "a",1,"b",False,"c" #tuple
+print(type(tuple1))
+tuple2 = ()
+tuple3 =("q",1,3,4,5.6,7j)
+tuple4 = (1,2,3,4,5) 
+print(len(tuple3))
+print(min(tuple4)) # can't use min max btw string and num but can individually
+print(max(tuple4))
