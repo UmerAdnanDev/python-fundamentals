f05daec0295dc93f34f3006ec147e42b716dc2d6
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-04-01 23:28:17 +0500

    added with keyword

---
+++ File Handling/withkeyword.py
@@ -0,0 +1,3 @@
+with open('C:/Users/InflightAdmin/Documents/Python/Exception Handling/customexception.py','r') as file:
+    print(file.read())
+# due to with function we donot need to close file as it auto closes it as the task is completed
