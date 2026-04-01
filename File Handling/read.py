690b31ef00b73cde19c72f632d72fda182bb18f5
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-04-01 23:40:18 +0500

    changed the file path

--- File Handling/read.py
+++ File Handling/read.py
@@ -1,4 +1,4 @@
-file = open('C:/Users/InflightAdmin/Documents/Python/File Handling/filetype.txt','r') # path,mode make sure the path has / not \
+file = open('C:/Users/InflightAdmin/Documents/GitHub/python-fundamentals/File Handling/filetype.txt','r') # path,mode make sure the path has / not \
 content = file.read()
 print(content)
 file.close()
--- File Handling/write.py
+++ File Handling/write.py
@@ -1,4 +1,4 @@
-file = open('C:/Users/InflightAdmin/Documents/Python/File Handling/filewrite.txt','w') # only allows writting and will create a new file if not already present and will override the data
+file = open('C:/Users/InflightAdmin/Documents/GitHub/python-fundamentals/File Handling/filewrite.txt','w') # only allows writting and will create a new file if not already present and will override the data
 content = input("Enter content to write in the file: ")
 file.write(content)
 print("Data Successfully Stored")
