230cf4a0d6c3f73fbb93d601e1e21cb71f746b81
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-04-01 23:26:17 +0500

    added file reading

---
+++ File Handling/read.py
@@ -0,0 +1,5 @@
+file = open('C:/Users/InflightAdmin/Documents/Python/File Handling/filetype.txt','r') # path,mode make sure the path has / not \
+content = file.read()
+print(content)
+file.close()
+print()
