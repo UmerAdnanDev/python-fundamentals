305c4a7fa761a408d400d8b6290e0b6a3fcb0622
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-04-02 20:45:08 +0500

    added classes and objects

---
+++ OOP/Classes & Objects/class1.py
@@ -0,0 +1,11 @@
+class Plane:
+    def fly(self):
+        print("The Plane is Flying")
+    def land(self):
+        print("The Plane is Landing")
+plane1 = Plane()
+plane2 = Plane()
+plane1.fly()
+plane1.land()        
+plane2.fly()
+plane2.land()
